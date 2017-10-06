6673c5bf4e4879ca77181e1aab922c03de12f8d8
Dan Jørgensen <djo@yoyoglobal.com>
2017-10-06 22:13:12 +0200

    init

---
+++ WLiteLogService/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WLiteLogService.Models;
+
+namespace WLiteLogService.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(UserProfile user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.Username == "admin" && user.Password == "12345678")
+                {
+                    RedirectToAction("Index", "Dashboard");
+                }
+            }
+            return View("~/Views/Login/Index.cshtml", user);
+        }
+    }
+}
---
+++ WLiteLogService/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WLiteLogService.Models
+{
+    public class UserProfile
+    {
+        [DisplayName("Username")]
+        public string Username { get; set; }
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
